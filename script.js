ed471fb2907556af50ee8a685cf7d833b292d006
rubyciide <joelpaul900@gmail.com>
2023-07-08 11:09:14 +0530

    improved logic made the js cleaner

--- script.js
+++ script.js
@@ -1,29 +1,16 @@
 const board = document.createElement('div');
-board.classList.add('board')
-var square;
-for(i = 0;i<16;i++){
-    const row = document.createElement('div');
-    row.style['display'] = 'flex';
-    row.style['background-color'] = 'coral';
-    row.classList.add('.row')
-    for( j = 0;j<16;j++){
-        square = document.createElement('div');
-        // square.style.backgroundColor = 'blue';
-        square.classList.add('square')
-        square.style.width='20px'
-        square.style.height='20px'
-        square.style['border'] = '1px solid black';
-        // square.style.margin = '10px'
-        row.appendChild(square);
-    }
-    board.appendChild(row);
-  }
-document.querySelector('.container').appendChild(board);
-const squares = document.querySelectorAll('.square');
 const button = document.getElementById('draw');
 const clear = document.getElementById('clear');
+const container =document.querySelector('.container'); 
+const change = document.getElementById('change');
+
+board.classList.add('board')
+changeGrids(16);
+container.appendChild(board);
+
 
 clear.addEventListener('click',()=>{
+  const squares = document.querySelectorAll('.square')
   squares.forEach((square)=>{
     square.style.backgroundColor="";
   })
@@ -34,12 +21,48 @@ button.addEventListener('click',()=>{
   button.classList.toggle('active');
 })
 
-squares.forEach((square) => {
 
-  square.addEventListener('mouseover', () => {
-    if(button.classList.contains('active')){
-      square.style.backgroundColor = 'red'; // Change the color when hovered over after clicking
+
+change.addEventListener('click',()=>{
+  try{
+  const a = prompt('Enter the no of grids in the pad?');
+  if(a<100 && !isNaN(a)) changeGrids(a);
+  else throw Error();
+  }
+  catch{
+    alert('Please enter a valid number!');
+  }
+})
+
+
+function changeGrids(a) {
+  const boardSize = 480; //since i put the height and width as 480 px in the css
+  const squareSize = boardSize / a;
+  board.innerHTML = "";
+  
+  for (let i = 0; i < a; i++) {
+    const row = document.createElement('div');
+    row.classList.add('row');
+
+    for (let j = 0; j < a; j++) {
+      const square = document.createElement('div');
+      square.classList.add('square');
+      square.style.width = `${squareSize}px`;
+      square.style.height = `${squareSize}px`;
+      square.style.border = '1px solid black';
+      row.appendChild(square);
     }
-  });
-});
 
+    board.appendChild(row);
+  }
+
+  const button = document.querySelector('#draw');
+  const squares = document.querySelectorAll('.square');
+  squares.forEach((square) => {
+    square.addEventListener('mouseover', () => {
+      if (button.classList.contains('active')) {
+        square.style.backgroundColor = 'red';
+      }
+    });
+  });
+}
